860915e44533ad92540ec239c39c072dc358043b
Lucasdias067 <lucasdias067@gmail.com>
2025-07-01 15:48:47 -0300

    commit

---
+++ src/infra/auth/roles.guard.ts
@@ -0,0 +1,29 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable
+} from '@nestjs/common'
+import { Reflector } from '@nestjs/core'
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.get<Role[]>(
+      'roles',
+      context.getHandler()
+    )
+
+    if (!requiredRoles) return true 
+
+    const { user } = context.switchToHttp().getRequest() 
+
+    if (!user || !user.roles) {
+      throw new ForbiddenException('User does not have roles')
+    }
+
+    return requiredRoles.some(role => user.roles.includes(role)) // Verifica se o usuário possui alguma das roles exigidas
+  }
+}
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
 import { JwtModules } from 'src/infra/auth/jwt.module'
+import { JwtStrategy } from 'src/infra/auth/jwt.strategy'
+import { JwtAuthGuard } from 'src/infra/auth/jwt-auth.guard'
+import { RolesGuard } from 'src/infra/auth/roles.guard'
 import { UserRepository } from '../users/domain/repositories/user.repository'
 import { PrismaUserRepository } from '../users/infra/database/repositories/prisma.user.repository'
 import { LoginUserUseCase } from './domain/use-cases/loginUserUseCase'
 import { AuthController } from './infra/http/controller/auth.controller'
 
 @Module({
-  imports: [JwtModules.forRoot()],
+  imports: [JwtModules.forRoot(), PassportModule],
   controllers: [AuthController],
   providers: [
     {
@@ -18,7 +22,11 @@ import { AuthController } from './infra/http/controller/auth.controller'
       provide: 'jwtService',
       useClass: JwtService
     },
-    LoginUserUseCase
-  ]
+    LoginUserUseCase,
+    JwtStrategy,
+    JwtAuthGuard,
+    RolesGuard
+  ],
+  exports: [JwtModules]
 })
 export class AuthModule {}
